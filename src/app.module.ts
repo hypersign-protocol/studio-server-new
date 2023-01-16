2d119e6898a772dcf6ff54e477eb5d354e9b4e18
vishwas1 <vishwasanand.cse@gmail.com>
2023-01-16 09:22:35 +0530

    Implmented env file st

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppAuthModule } from './app-auth/app-auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     AppAuthModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/studio-api'),
+    ConfigModule.forRoot({
+      envFilePath: '.dev.env'
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_CONNECTION_PATH)
   ],
   controllers: [],
   providers: [],
