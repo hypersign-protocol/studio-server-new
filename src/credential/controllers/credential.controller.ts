f832f29fae0d8441a4eccfdc53cd3ef0cc790a7a
varsha <varshakumari370@gmail.com>
2023-01-25 22:34:55 +0530

    resolved credential detail

--- src/credential/controllers/credential.controller.ts
+++ src/credential/controllers/credential.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  UseGuards,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CredentialService } from '../services/credential.service';
 import { CreateCredentialDto } from '../dto/create-credential.dto';
 import { UpdateCredentialDto } from '../dto/update-credential.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  CredentialError,
+  CredentialNotFoundError,
+} from '../dto/error-credential.dto';
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-
 
 @ApiBearerAuth('Authorization')
 @UseGuards(AuthGuard('jwt'))
@@ -14,30 +35,68 @@ export class CredentialController {
   constructor(private readonly credentialService: CredentialService) {}
 
   @Get()
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Error in finding resource',
+    type: CredentialNotFoundError,
+  })
+  @ApiOkResponse({
+    description: 'List of credentials',
+    type: String,
+    isArray: true,
+  })
   findAll(@Req() req) {
-    
     return this.credentialService.findAll(req.user);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.credentialService.findOne(+id);
+  @Get(':credentialId')
+  @ApiOkResponse({
+    description: 'Resolved credential detail',
+    // type: ,
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'vc:hid:testnet:......',
+    type: CredentialNotFoundError,
+  })
+  resolveCredential(
+    @Req() req: any,
+    @Param('credentialId') credentialId: string,
+  ) {
+    const appDetail = req.user;
+    return this.credentialService.resolveCredential(credentialId, appDetail);
   }
-
 
   @UsePipes(ValidationPipe)
   @Post()
-  create(@Body() createCredentialDto: CreateCredentialDto,@Req() req) {    
-    return this.credentialService.create(createCredentialDto,req.user);
+  @ApiCreatedResponse({
+    description: 'Credential Created',
+    type: Object,
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Error occured at the time of creating schema',
+    type: CredentialError,
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Resource not found',
+    type: CredentialNotFoundError,
+  })
+  create(@Body() createCredentialDto: CreateCredentialDto, @Req() req) {
+    return this.credentialService.create(createCredentialDto, req.user);
   }
 
   @Post('/verify')
-  verify(@Body() createCredentialDto: CreateCredentialDto,@Req() req) {
-    return this.credentialService.create(createCredentialDto,req.user);
+  verify(@Body() createCredentialDto: CreateCredentialDto, @Req() req) {
+    return this.credentialService.create(createCredentialDto, req.user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCredentialDto: UpdateCredentialDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCredentialDto: UpdateCredentialDto,
+  ) {
     return this.credentialService.update(+id, updateCredentialDto);
   }
 }
--- src/credential/services/credential.service.ts
+++ src/credential/services/credential.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCredentialDto } from '../dto/create-credential.dto';
 import { UpdateCredentialDto } from '../dto/update-credential.dto';
 import { ConfigService } from '@nestjs/config';
@@ -7,13 +11,30 @@ import { HidWalletService } from 'src/hid-wallet/services/hid-wallet.service';
 import { CredentialRepository } from '../repository/credential.repository';
 import { EdvService } from 'src/edv/services/edv.service';
 import { DidRepository } from 'src/did/repository/did.repository';
-import { HypersignDID } from 'hs-ssi-sdk'
+import { HypersignDID, HypersignVerifiableCredential } from 'hs-ssi-sdk';
 @Injectable()
 export class CredentialService {
-  constructor(private readonly config: ConfigService, private readonly credentialSSIService: CredentialSSIService, private readonly edvService: EdvService,
-    private readonly hidWallet: HidWalletService, private credentialRepository: CredentialRepository, private readonly didRepositiory: DidRepository) { }
+  constructor(
+    private readonly config: ConfigService,
+    private readonly credentialSSIService: CredentialSSIService,
+    private readonly edvService: EdvService,
+    private readonly hidWallet: HidWalletService,
+    private credentialRepository: CredentialRepository,
+    private readonly didRepositiory: DidRepository,
+  ) {}
   async create(createCredentialDto: CreateCredentialDto, appDetail) {
-    const { schemaId, subjectDid, subjectDidDocSigned, schemaContext, type, issuerDid, expirationDate, fields, verificationMethodId, persist } = createCredentialDto
+    const {
+      schemaId,
+      subjectDid,
+      subjectDidDocSigned,
+      schemaContext,
+      type,
+      issuerDid,
+      expirationDate,
+      fields,
+      verificationMethodId,
+      persist,
+    } = createCredentialDto;
     const nameSpace = createCredentialDto.namespace;
     const didOfvmId = verificationMethodId.split('#')[0];
 
@@ -32,9 +53,11 @@ export class CredentialService {
     }
     const docs = await this.edvService.getDecryptedDocument(edvDocId);
     const mnemonic: string = docs.mnemonic;
-    await this.hidWallet.generateWallet(mnemonic)
+    await this.hidWallet.generateWallet(mnemonic);
     try {
-      const slipPathKeys = this.hidWallet.makeSSIWalletPath(didInfo.hdPathIndex);
+      const slipPathKeys = this.hidWallet.makeSSIWalletPath(
+        didInfo.hdPathIndex,
+      );
 
       const seed = await this.hidWallet.generateMemonicToSeedFromSlip10RawIndex(
         slipPathKeys,
@@ -43,38 +66,77 @@ export class CredentialService {
       const hypersignDid = new HypersignDID();
 
       const { privateKeyMultibase } = await hypersignDid.generateKeys({ seed });
-
-      const hypersignVC = await this.credentialSSIService.initateHypersignVC(mnemonic, nameSpace)
-      const credential = await hypersignVC.generate({ schemaId, subjectDid, issuerDid, fields, expirationDate })
-
-      const { signedCredential, credentialStatus, credentialStatusProof, credentialStatusRegistrationResult } = await hypersignVC.issue({ credential, issuerDid, verificationMethodId, privateKeyMultibase })
-      let edvData = undefined
+      const hypersignVC = await this.credentialSSIService.initateHypersignVC(
+        mnemonic,
+        nameSpace,
+      );
+      const credential = await hypersignVC.generate({
+        schemaId,
+        subjectDid,
+        issuerDid,
+        fields,
+        expirationDate,
+      });
+      const {
+        signedCredential,
+        credentialStatus,
+        credentialStatusProof,
+        credentialStatusRegistrationResult,
+      } = await hypersignVC.issue({
+        credential,
+        issuerDid,
+        verificationMethodId,
+        privateKeyMultibase,
+      });
+      let edvData = undefined;
       if (persist) {
-        edvData = await this.edvService.createDocument({ signedCredential, credentialStatus, credentialStatusProof, credentialStatusRegistrationResult })
+        edvData = await this.edvService.createDocument({
+          signedCredential,
+          credentialStatus,
+          credentialStatusProof,
+          credentialStatusRegistrationResult,
+        });
       }
       await this.credentialRepository.create({
         appId: appDetail.appId,
         credentialId: signedCredential.id,
         issuerDid,
         persist: persist,
-        edvDocId: edvData && edvData.id ? edvData.id : "",
-        transactionHash: credentialStatusRegistrationResult.transactionHash
-      })
+        edvDocId: edvData && edvData.id ? edvData.id : '',
+        transactionHash: credentialStatusRegistrationResult.transactionHash,
+      });
       return { credential: signedCredential, credentialStatus, persist };
     } catch (e) {
       throw new BadRequestException([e.message]);
-
     }
-
   }
 
   findAll(appDetail) {
-
-    return this.credentialRepository.find({ appId: appDetail.appId })
+    return this.credentialRepository.find({ appId: appDetail.appId });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} credential`;
+  async resolveCredential(credentialId: string, appDetail) {
+    const credentialDetail = await this.credentialRepository.findOne({
+      appId: appDetail.appId,
+      credentialId,
+    });
+    if (!credentialDetail || credentialDetail == null) {
+      throw new NotFoundException([
+        `${credentialId} is not found`,
+        `${credentialId} does not belongs to the App id: ${appDetail.appId}`,
+      ]);
+    }
+    const hypersignCredential = new HypersignVerifiableCredential();
+    let resolvedCredential;
+    try {
+      resolvedCredential = await hypersignCredential.resolveCredentialStatus({
+        credentialId,
+      });
+    } catch (e) {
+      throw new BadRequestException([e.message]);
+    }
+
+    return resolvedCredential;
   }
 
   update(id: number, updateCredentialDto: UpdateCredentialDto) {
