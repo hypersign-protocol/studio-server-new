8383eee1948ca1b2038b9e64629ea007ede631c8
varsha <varshakumari370@gmail.com>
2023-01-30 16:38:14 +0530

    added swagger request

--- src/presentation/dto/create-presentation.dto.ts
+++ src/presentation/dto/create-presentation.dto.ts
@@ -1,10 +1,171 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsString, IsUrl, ValidateNested } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
+import {
+  CredentialSchema,
+  CredentialSubject,
+} from 'src/credential/dto/create-credential.dto';
+import { IsDid } from 'src/schema/decorator/schema.decorator';
 import { ToSnakeCase } from 'src/utils/customDecorator/case.decorator';
 import { Trim } from 'src/utils/customDecorator/trim.decorator';
-import { Query } from '../schemas/presentation-template.schema';
+import { queryType } from '../schemas/presentation-template.schema';
+export class TruestedIssuer {
+  @ApiProperty({
+    name: 'required',
+    description: 'defining property is required or not',
+    example: true,
+  })
+  @IsBoolean()
+  required: boolean;
+  @ApiProperty({
+    name: 'issuer',
+    description: 'did of the issuer',
+    example: 'did:hid:testnet:................',
+  })
+  @IsDid()
+  issuer: string;
+}
 
+export class QueryExample {
+  @ApiProperty({
+    name: 'context',
+    description: 'Context',
+    example: ['https://www.w3.org/2018/credentials/v1'],
+  })
+  @IsArray()
+  '@context': Array<string>;
+
+  @ApiProperty({
+    name: 'type',
+    description: 'Type of schema used',
+    example: 'AdharCard',
+  })
+  @IsString()
+  type: string;
+
+  @ApiProperty({
+    name: 'CredentialSubject',
+    description: 'Credential subject',
+    example: {
+      name: 'Random name',
+      id: 'did:hid:testnet:.............................',
+    },
+  })
+  @ValidateNested()
+  @Type(() => CredentialSubject)
+  credentialSubject: CredentialSubject;
+
+  @ApiProperty({
+    name: 'credentialSchema',
+    description: 'Schema detail based on which credential has issued',
+    example: {
+      name: 'sch:hid:testnet:...........',
+      id: 'JsonSchemaValidator2018',
+    },
+  })
+  @ValidateNested()
+  @Type(() => CredentialSchema)
+  credentialSchema: CredentialSchema;
+  @ApiProperty({
+    name: 'trustedIssuer',
+    description: 'Specify credentials from a particular issuer only',
+    example: {
+      required: true,
+      issuer: 'did:hid:testnet:................',
+    },
+  })
+  @Type(() => TruestedIssuer)
+  @ValidateNested()
+  trustedIssuer: Array<TruestedIssuer>;
+}
+
+export class CredentialQuery {
+  @ApiProperty({
+    name: 'required',
+    description: 'False by default',
+    example: true,
+  })
+  @IsBoolean()
+  required: boolean;
+  @ApiProperty({
+    name: 'reason',
+    description: 'Reason for requesting this credential ',
+    example: 'We need you to prove your eligibility to work.',
+  })
+  @IsString()
+  reason: string;
+  @ApiProperty({
+    name: 'example',
+    description:
+      'sample detail of credential for which presentation template is to be created',
+    example: {
+      '@context': ['https://www.w3.org/2018/credentials/v1'],
+      type: 'AdharCard',
+      credentialSubject: {
+        name: 'Random name',
+        id: 'did:hid:testnet:.............................',
+      },
+      credentialSchema: {
+        name: 'sch:hid:testnet:...........',
+        id: 'JsonSchemaValidator2018',
+      },
+      TruestedIssuer: {
+        required: true,
+        issuer: 'did:hid:testnet:................',
+      },
+    },
+  })
+  @ValidateNested()
+  @Type(() => QueryExample)
+  example: QueryExample;
+}
+export class Query {
+  @ApiProperty({
+    name: 'type',
+    description: 'Query Type',
+    example: 'QueryByExample / DIDAuthentication',
+  })
+  @IsString()
+  type: queryType;
+
+  @ApiProperty({
+    name: 'credentialQuery',
+    description: 'credentialQuery',
+    example: [
+      {
+        required: true,
+        reason: 'We need you to prove your eligibility to work.',
+        example: {
+          '@context': ['https://www.w3.org/2018/credentials/v1'],
+          type: 'AdharCard',
+          credentialSubject: {
+            name: 'Random name',
+            id: 'did:hid:testnet:.............................',
+          },
+          credentialSchema: {
+            name: 'sch:hid:testnet:...........',
+            id: 'JsonSchemaValidator2018',
+          },
+          TruestedIssuer: {
+            required: true,
+            issuer: 'did:hid:testnet:................',
+          },
+        },
+      },
+    ],
+  })
+  @IsArray()
+  @ValidateNested()
+  @Type(() => CredentialQuery)
+  credentialQuery: Array<CredentialQuery>;
+}
 export class CreatePresentationTemplateDto {
   @ApiProperty({
     name: 'domain',
@@ -22,14 +183,42 @@ export class CreatePresentationTemplateDto {
     description: 'name of the presentation template',
     example: 'alumni_credential_request (sanke_case)',
   })
-
   @IsString()
   @IsNotEmpty()
   @ToSnakeCase()
   name: string;
-
-  @ValidateNested()
+  @ApiProperty({
+    name: 'query',
+    description: 'query parameter for requesting presentation',
+    example: [
+      {
+        type: 'QueryByExample',
+        credentialQuery: [
+          {
+            required: true,
+            reason: 'We need you to prove your eligibility to work.',
+            example: {
+              '@context': ['https://www.w3.org/2018/credentials/v1'],
+              type: 'AdharCard',
+              credentialSubject: {
+                name: 'Random name',
+                id: 'did:hid:testnet:.............................',
+              },
+              credentialSchema: {
+                name: 'sch:hid:testnet:...........',
+                id: 'JsonSchemaValidator2018',
+              },
+              TruestedIssuer: {
+                required: true,
+                issuer: 'did:hid:testnet:................',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  })
   @Type(() => Query)
-  query: Array<Query>
-
+  @ValidateNested()
+  query: Query;
 }
--- src/presentation/schemas/presentation-template.schema.ts
+++ src/presentation/schemas/presentation-template.schema.ts
@@ -1,119 +1,105 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsArray, IsBoolean, IsEnum, IsObject, IsString, IsUrl, ValidateNested } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsObject,
+  IsString,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
 import { Document } from 'mongoose';
-import { CreateCredentialDto, CredentialSchema, CredentialSubject } from 'src/credential/dto/create-credential.dto';
+import {
+  CreateCredentialDto,
+  CredentialSchema,
+  CredentialSubject,
+} from 'src/credential/dto/create-credential.dto';
 import { IsDid } from 'src/schema/decorator/schema.decorator';
 import { ToPascalCase } from 'src/utils/customDecorator/case.decorator';
 
 export type PresentationTemplateDocument = PresentationTemplate & Document;
 
-
-
-
-
 export enum queryType {
-  QueryByExample = "QueryByExample",
-  QueryByFrame = "QueryByFrame",
-  DIDAuthentication = "DIDAuthentication"
+  QueryByExample = 'QueryByExample',
+  QueryByFrame = 'QueryByFrame',
+  DIDAuthentication = 'DIDAuthentication',
 }
 export class Query {
   @ApiProperty({
-    name: "type",
-    description: "Query Type",
-    example: "QueryByExample / DIDAuthentication"
+    name: 'type',
+    description: 'Query Type',
+    example: 'QueryByExample / DIDAuthentication',
   })
   @IsString()
   @ToPascalCase()
   @Prop()
   @IsEnum(queryType)
-  type: queryType
-
+  type: queryType;
 
   @IsArray()
   @ValidateNested()
   @Type(() => CredentialQuery)
   @Prop({ required: true })
-  credentialQuery: Array<CredentialQuery>
-
-
-
+  credentialQuery: Array<CredentialQuery>;
 }
 
 export class TruestedIssuer {
-
   @IsBoolean()
   @Prop()
-  required: boolean
+  required: boolean;
   @IsDid()
   @Prop()
-  issuer: string
+  issuer: string;
 }
 
 export class QueryExample {
-
-
   @IsArray()
   @Prop()
-  "@context": Array<String>
+  '@context': Array<String>;
 
   @IsString()
   @ToPascalCase()
   @Prop()
-  type: string
+  type: string;
 
   @ValidateNested()
   @Type(() => CredentialSubject)
   @Prop({ required: false })
-  credentialSubject: CredentialSubject
-
-
+  credentialSubject: CredentialSubject;
 
   @ValidateNested()
   @Type(() => CredentialSchema)
   @Prop({ required: false })
-  credentialSchema: CredentialSchema
-
+  credentialSchema: CredentialSchema;
 
   @Type(() => TruestedIssuer)
   @ValidateNested()
   @Prop()
-  trustedIssuer: Array<TruestedIssuer>
-
-
-
+  trustedIssuer: Array<TruestedIssuer>;
 }
 
 export class CredentialQuery {
-
-
   @IsBoolean()
   @Prop({ required: false, default: true })
-  required: boolean
+  required: boolean;
 
   @IsString()
-  @Prop({ required: false, default: "We need you to prove your eligibility.." })
-  reason: string
+  @Prop({ required: false, default: 'We need you to prove your eligibility..' })
+  reason: string;
 
   @ValidateNested()
   @Type(() => QueryExample)
   @Prop()
-  example: QueryExample
-
-
-
+  example: QueryExample;
 }
-
-
-
 
 @Schema()
 export class PresentationTemplate {
   @IsString()
   @Prop({ required: true })
   appId: string;
-
 
   @IsString()
   @IsUrl({ protocols: ['http', 'https'] })
@@ -128,10 +114,8 @@ export class PresentationTemplate {
   @ValidateNested()
   @Type(() => Query)
   @Prop({ required: true })
-  query: Array<Query>
+  query: Array<Query>;
 }
-
-
 
 const presentationTemplateSchema =
   SchemaFactory.createForClass(PresentationTemplate);
