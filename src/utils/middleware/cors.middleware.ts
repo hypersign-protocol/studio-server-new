c836572e18912d9a5ec3904f5f2780261d0bcb8b
varsha <varshakumari370@gmail.com>
2023-02-08 23:57:06 +0530

    added cors option in all api

---
+++ src/utils/middleware/cors.middleware.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+import { NextFunction, Request, Response } from 'express';
+import { AppRepository } from 'src/app-auth/repositories/app.repository';
+@Injectable()
+export class WhitelistMiddleware implements NestMiddleware {
+  constructor(private readonly appRepositiory: AppRepository) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const whitelistedOrigins = process.env.WHITELISTED_CORS;
+    const origin = req.header('Origin');
+    if (whitelistedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    } else if (req.header('authorization')) {
+      const token = req.header('authorization').split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const appInfo = await this.appRepositiory.findOne({
+        appId: decoded['appId'],
+        userId: decoded['userId'],
+      });
+      if (!appInfo['whitelistedCors'].includes(origin)) {
+        throw new UnauthorizedException(['Origin mismatch']);
+      }
+    } else {
+      throw new UnauthorizedException(['This is a cors enable api']);
+    }
+    next();
+  }
+}
