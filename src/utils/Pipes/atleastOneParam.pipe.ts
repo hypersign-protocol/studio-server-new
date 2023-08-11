ad374acbe87b0d86cc573144bec0bba6a0d552a1
varsha766 <varshakumari370@gmail.com>
2023-08-11 12:50:32 +0530

    code prettifier

--- src/utils/Pipes/atleastOneParam.pipe.ts
+++ src/utils/Pipes/atleastOneParam.pipe.ts
@@ -1,12 +1,8 @@
-import {
-  PipeTransform,
-  Injectable,
-  BadRequestException,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class AtLeastOneParamPipe implements PipeTransform {
-  constructor(private readonly allowedParams: string[]) { }
+  constructor(private readonly allowedParams: string[]) {}
   transform(value: any) {
     const presentParams = this.allowedParams.filter((param) => value[param]);
     if (presentParams.length === 0) {
