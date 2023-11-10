0e4c65e17668f32072773ddb2b3b07621c4f8243
varsha766 <varshakumari370@gmail.com>
2023-11-10 20:14:23 +0530

    code prettified

--- src/app-auth/strategy/jwt.strategy.ts
+++ src/app-auth/strategy/jwt.strategy.ts
@@ -3,13 +3,11 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AppRepository } from '../repositories/app.repository';
-import { AppAuthSecretService } from '../services/app-auth-passord.service';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     private readonly config: ConfigService,
     private readonly appRepository: AppRepository,
-    private readonly appAuthSecretService: AppAuthSecretService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -22,24 +20,5 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       appId: payload.appId,
     });
     return appDetail;
-  }
-}
-
-@Injectable()
-export class JwtStrategyApp extends PassportStrategy(Strategy, 'jwtApp') {
-  constructor(
-    private readonly config: ConfigService,
-    private readonly appRepository: AppRepository,
-    private readonly appAuthSecretService: AppAuthSecretService,
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: config.get('STUDIO_SERVER_JWT_SECRET'),
-    });
-  }
-  async validate(payload) {
-    payload.userId = payload.email;
-    return payload;
   }
 }
