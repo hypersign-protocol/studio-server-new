536faea75e4b0be75a15b665869d4782069d897e
varsha766 <varshakumari370@gmail.com>
2023-11-15 13:28:43 +0530

    added testcase and postman collection

--- src/app-oauth/app-oauth.module.ts
+++ src/app-oauth/app-oauth.module.ts
@@ -1,14 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppOauthController } from './app-oauth.controller';
 import { AppAuthModule } from 'src/app-auth/app-auth.module';
-import { WhitelistSSICorsMiddleware } from 'src/utils/middleware/cors.middleware';
 @Module({
   imports: [AppAuthModule],
   controllers: [AppOauthController],
   providers: [],
 })
-export class AppOauthModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(WhitelistSSICorsMiddleware).forRoutes(AppOauthController);
-  }
-}
+export class AppOauthModule {}
