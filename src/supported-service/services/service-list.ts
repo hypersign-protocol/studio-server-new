0e81db8d239fe2546407c4494ebe1a75dffbf31f
Pratap2018 <pratapmridha@gmail.com>
2024-08-29 18:37:48 +0530

    update serviceList

--- src/supported-service/services/service-list.ts
+++ src/supported-service/services/service-list.ts
@@ -9,7 +9,6 @@ type Serivce = {
   description: string;
   swaggerAPIDocPath: string;
   accessList: any;
-
 };
 
 @Injectable()
@@ -26,7 +25,6 @@ export class SupportedServiceList {
         description: SERVICE_INFO.SSI_API.description,
         swaggerAPIDocPath: SERVICE_INFO.SSI_API.swaggerAPIDocPath,
         accessList: SERVICES.SSI_API.ACCESS_TYPES,
-
       },
       {
         id: SERVICE_INFO.CAVACH_API.type,
@@ -37,7 +35,6 @@ export class SupportedServiceList {
           SERVICE_INFO.CAVACH_API.baseDomain,
         description: SERVICE_INFO.CAVACH_API.description,
         swaggerAPIDocPath: SERVICE_INFO.CAVACH_API.swaggerAPIDocPath,
-
         accessList: SERVICES.CAVACH_API.ACCESS_TYPES,
       },
       {
@@ -48,7 +45,6 @@ export class SupportedServiceList {
         description: SERVICE_INFO.DASHBOARD.description,
         swaggerAPIDocPath: SERVICE_INFO.DASHBOARD.swaggerAPIDocPath,
         accessList: SERVICES.DASHBOARD.ACCESS_TYPES,
-        accessList: SERVICES.CAVACH_API.ACCESS_TYPES
       },
     ];
   }
