fb0033ef3b1d5e20a63e223fdb599f01f11228e8
varsha766 <varshakumari370@gmail.com>
2024-09-14 11:45:07 +0530

    added function to provide default credit for cavach service

--- src/credits/services/credits.service.ts
+++ src/credits/services/credits.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { AuthZCreditsRepository } from '../repositories/authz.repository';
 import { scope } from '../../credits/schemas/authz.schema';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { APP_ENVIRONMENT } from 'src/supported-service/services/iServiceList';
 
 @Injectable()
 export class AuthzCreditService {
   constructor(
     private readonly authzCreditsRepository: AuthZCreditsRepository,
     private readonly config: ConfigService,
+    private readonly jwt: JwtService,
   ) {}
 
   async createAuthzCredits(authz: { userId; appId }) {
@@ -35,6 +38,41 @@ export class AuthzCreditService {
     return this.authzCreditsRepository.find({
       userId,
       appId,
+    });
+  }
+
+  async grantCavachCredit(
+    subdomain: string,
+    appId: string,
+    environmentMode: APP_ENVIRONMENT,
+    tenantUrl: string,
+  ) {
+    Logger.log(
+      'grantCavachCredit() method to grant free credit to cavach service',
+      'AuthZCreditService',
+    );
+    const payload = {
+      purpose: 'CreditRecharge',
+      amount: 2100, // will take this as function parameter later
+      amountDenom: 'uHID',
+      validityPeriod: 6,
+      serviceId: appId,
+      validityPeriodUnit: 'MONTH',
+      env: environmentMode,
+      subdomain,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+    fetch(`${tenantUrl}api/v1/credit`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+        authorization: `Bearer ${token}`,
+      },
+    }).catch((error) => {
+      Logger.error('Failed to grant credit:', error);
     });
   }
 }
